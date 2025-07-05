e62b81a66dc1779199f10902bae1bf246a9b7d3f
sahAlves <sah.alves002@gmail.com>
2025-07-04 22:28:29 -0300

    Fazendo autenticação

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -2,4 +2,28 @@
 
 $mensagem = $_REQUEST['mensagem'] ?? '';  
 
+// 1. Receber o formulário com email e senha
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+// 2. Fazer uma consulta no banco de dados com o email e senha
+    $usuario = $database
+                    ->query(
+                    query: "SELECT * 
+                        FROM usuarios
+                        WHERE email = :email
+                        AND senha = :senha",
+                    params: compact('email', 'senha'))
+                    ->fetch();
+                    
+    if($usuario) {
+        // 3. Se existir, nós vamos adicionar na sessão que o usuário está autenticado
+        $_SESSION['auth'] = $usuario;
+        $_SESSION['mensagem'] = 'Seja bem vindo ' . $usuario['nome'] . '!';
+        header('Location: /');
+        exit();
+    }
+}
+
 view('login', compact('mensagem'));
--- views/login.view.php
+++ views/login.view.php
@@ -1,7 +1,7 @@
 <div class="mt-6 grid grid-cols-2 gap-2">
     <div class="border border-stone-700 rounded">
         <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Login</h1>
-        <form class="p-4 space-y-4">
+        <form class="p-4 space-y-4" method="POST">
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Email</label>
                 <input
